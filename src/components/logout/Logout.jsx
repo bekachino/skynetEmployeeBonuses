04b737d4ac723d54295b37e973abc8fbbc1f025e
Bektur Razzakov <brzzkv@gmail.com>
2024-03-18 18:55:36 +0600

    Logout button UI

---
+++ src/components/logout/Logout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './logout.css';
+
+const Logout = () => {
+  return (
+    <button type='button' className="logout-btn">
+      <div className="logout-btn-icon"/>
+      Выйти
+    </button>
+  );
+};
+
+export default Logout;
