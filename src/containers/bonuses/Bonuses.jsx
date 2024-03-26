c176ab0f3532906041fc0acc2fc2a897dae99d4d
Bektur Razzakov <brzzkv@gmail.com>
2024-03-26 15:27:24 +0600

    Calculations updated

--- src/containers/bonuses/Bonuses.jsx
+++ src/containers/bonuses/Bonuses.jsx
@@ -12,24 +12,69 @@ const Bonuses = () => {
   const [state, setState] = useState({
     date: '',
     district: '',
+  });
+  const [data, setData] = useState({
     actives: 3000,
     nonActives: 450,
-    bonusPerAbonent: 150,
+    bonusPerActiveAbonent: 150,
+    currentPercentage: 90,
+    bonusPerConnectedAbonent980: 588,
+    bonusPerConnectedAbonent1200: 720,
+    connectedAbonents980: 50,
+    connectedAbonents1200: 30,
+    bonusPerConnectedAbonent: 1000,
+    bonusPerActiveAbonent2: 25,
+    bonusPerReturnedAbonent2: 25,
   });
   const [formLoading, setFormLoading] = useState(false);
-  const [currentPercentage, setCurrentPercentage] = useState(90);
-  const activesPercentage = (state.actives / (state.actives + state.nonActives) * 100).toFixed(2) || 0;
-  const abonentsPlusMinusPercentage = (activesPercentage - currentPercentage).toFixed(2) || 0;
-  const abonentsPlusMinusNumber = (state.actives / (activesPercentage / currentPercentage))
-    .toFixed(0) - state.actives;
+  const activesPercentage = Number((data.actives / (data.actives + data.nonActives) * 100).toFixed(2)) || 0;
+  const abonentsPlusMinusPercentage = Number((activesPercentage - data.currentPercentage).toFixed(2)) || 0;
+  const abonentsPlusMinusNumber = Math.floor((abonentsPlusMinusPercentage / 100) * (data.actives + data.nonActives));
+  const connected980Bonuses = data.connectedAbonents980 * data.bonusPerConnectedAbonent980;
+  const connected1200Bonuses = data.connectedAbonents1200 * data.bonusPerConnectedAbonent1200;
+
+  const q = data.actives + data.nonActives;
+  const w = connected980Bonuses + connected1200Bonuses;
+  const e = (data.connectedAbonents980 + data.connectedAbonents1200) * data.bonusPerConnectedAbonent;
+  const r = (abonentsPlusMinusNumber * data.bonusPerActiveAbonent > 0 ?
+      abonentsPlusMinusNumber * data.bonusPerActiveAbonent : 0) +
+    connected980Bonuses + connected1200Bonuses +
+    (data.connectedAbonents980 + data.connectedAbonents1200)
+    * data.bonusPerConnectedAbonent +
+    data.actives * data.bonusPerActiveAbonent2;
+  const bonusForCurrentMonth = (abonentsPlusMinusNumber * data.bonusPerActiveAbonent > 0 ?
+      abonentsPlusMinusNumber * data.bonusPerActiveAbonent : 0) +
+    connected980Bonuses + connected1200Bonuses +
+    (data.connectedAbonents980 + data.connectedAbonents1200)
+    * data.bonusPerConnectedAbonent +
+    data.actives * data.bonusPerActiveAbonent2;
 
   const changeHandler = (e) => {
     const {name, value} = e.target;
-    setState(prevState => ({
-      ...prevState,
-      [name]: value,
-    }));
+    if (
+      [
+        'bonusPerActiveAbonent', 'currentPercentage', 'bonusPerConnectedAbonent980',
+        'bonusPerConnectedAbonent1200', 'bonusPerConnectedAbonent', 'bonusPerActiveAbonent2',
+        'bonusPerReturnedAbonent2'
+      ].includes(name)
+    ) {
+      setData(prevState => ({
+        ...prevState,
+        [name]: value,
+      }));
+    } else {
+      setState(prevState => ({
+        ...prevState,
+        [name]: value,
+      }));
+    }
   };
+
+  const formatNumber = (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  };
+
+  const abonentAmountToReturn = Math.ceil(q * 0.03 * 150 + w + e + q * 0.93 * data.bonusPerReturnedAbonent2 - r);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -81,28 +126,30 @@ const Bonuses = () => {
             <div style={{margin: '0 2px 0 7px', display: 'flex', flexDirection: 'column'}}>
               <span
                 className="currentPercentage-increase"
-                onClick={() => (currentPercentage < 100) && setCurrentPercentage(currentPercentage + 1)}
+                onClick={() => (data.currentPercentage < 100) &&
+                  changeHandler({target: {name: 'currentPercentage', value: data.currentPercentage + 1}})}
               >&#x25B2;</span>
               <span
                 className="currentPercentage-decrease"
-                onClick={() => (currentPercentage > 0) && setCurrentPercentage(currentPercentage - 1)}
+                onClick={() => (data.currentPercentage > 0) &&
+                  changeHandler({target: {name: 'currentPercentage', value: data.currentPercentage - 1}})}
               >&#x25BC;</span>
             </div>
-            {' ' + currentPercentage}%
+            {' ' + data.currentPercentage}%
             {' '}Активных абонентов
           </h1>
           <div className="bonuses-table bonuses-table-1">
             <div className="table-col">
               <span className="table-col-title">ААБ</span>
-              <span className="table-col-value">{state.actives}</span>
+              <span className="table-col-value">{data.actives}</span>
             </div>
             <div className="table-col">
               <span className="table-col-title">НАБ</span>
-              <span className="table-col-value">{state.nonActives}</span>
+              <span className="table-col-value">{data.nonActives}</span>
             </div>
             <div className="table-col">
               <span className="table-col-title">ОАБ</span>
-              <span className="table-col-value">{state.actives + state.nonActives}</span>
+              <span className="table-col-value">{data.actives + data.nonActives}</span>
             </div>
             <div className="table-col">
               <span className="table-col-title">ААБ/ОАБ %</span>
@@ -111,7 +158,9 @@ const Bonuses = () => {
               </span>
             </div>
             <div className="table-col">
-              <span className="table-col-title">отклонение %</span>
+              <span className="table-col-title">
+                {abonentsPlusMinusPercentage < 0 ? 'отклонение' : 'соответствие'} %
+              </span>
               <span className={
                 `table-col-value
                 ${abonentsPlusMinusPercentage >= 0 ? 'table-col-value-plus' : 'table-col-value-minus'}`}
@@ -120,16 +169,14 @@ const Bonuses = () => {
               </span>
             </div>
             <div className="table-col">
-              <span className="table-col-title">отклонение, кол-во</span>
+              <span className="table-col-title">
+                {abonentsPlusMinusNumber < 0 ? 'отклонение' : 'соответствие'}, кол-во
+              </span>
               <span className={
                 `table-col-value
-                ${abonentsPlusMinusNumber <= 0 ? 'table-col-value-plus' : 'table-col-value-minus'}`}
+                ${abonentsPlusMinusNumber >= 0 ? 'table-col-value-plus' : 'table-col-value-minus'}`}
               >
-                {abonentsPlusMinusNumber < 0 ? '+' : abonentsPlusMinusNumber > 0 ? '-' : ''}
-                {Number(
-                  abonentsPlusMinusNumber > 0 ? abonentsPlusMinusNumber :
-                    `${abonentsPlusMinusNumber}`.slice(1, `${abonentsPlusMinusNumber}`.length)
-                )}
+                {abonentsPlusMinusNumber}
               </span>
             </div>
             <div className="table-col">
@@ -137,12 +184,16 @@ const Bonuses = () => {
                 Премия
                 <input
                   className="editable-input" type="text"
-                  name="bonusPerAbonent"
-                  value={state.bonusPerAbonent}
+                  name="bonusPerActiveAbonent"
+                  value={data.bonusPerActiveAbonent}
                   onChange={changeHandler}/>
                 <span style={{textDecoration: 'underline'}}>c</span>
               </span>
-              <span className="table-col-value">{abonentsPlusMinusNumber * state.bonusPerAbonent}</span>
+              <span className="table-col-value">
+                {
+                  abonentsPlusMinusNumber * data.bonusPerActiveAbonent > 0 ?
+                    formatNumber(abonentsPlusMinusNumber * data.bonusPerActiveAbonent) : 0}
+              </span>
             </div>
           </div>
         </div>
@@ -154,20 +205,42 @@ const Bonuses = () => {
               <span className="table-col-value">50</span>
             </div>
             <div className="table-col">
-              <span className="table-col-title">Сумма</span>
-              <span className="table-col-value">29400</span>
+              <span className="table-col-title">
+                Сумма
+                <input
+                  className="editable-input" type="text"
+                  name="bonusPerConnectedAbonent980"
+                  value={data.bonusPerConnectedAbonent980}
+                  onChange={changeHandler}/>
+                <span style={{textDecoration: 'underline'}}>c</span>
+              </span>
+              <span className="table-col-value">
+                {formatNumber(connected980Bonuses)}
+              </span>
             </div>
             <div className="table-col">
               <span className="table-col-title">Кол-во заявок ТП 1200</span>
               <span className="table-col-value">30</span>
             </div>
             <div className="table-col">
-              <span className="table-col-title">Кол-во заявок ТП 1200</span>
-              <span className="table-col-value">21600</span>
+              <span className="table-col-title">
+                Сумма
+                <input
+                  className="editable-input" type="text"
+                  name="bonusPerConnectedAbonent1200"
+                  value={data.bonusPerConnectedAbonent1200}
+                  onChange={changeHandler}/>
+                <span style={{textDecoration: 'underline'}}>c</span>
+              </span>
+              <span className="table-col-value">
+                {formatNumber(connected1200Bonuses)}
+              </span>
             </div>
             <div className="table-col">
               <span className="table-col-title table-col-value-yellow">Премия</span>
-              <span className="table-col-value">51 000</span>
+              <span className="table-col-value">
+                {formatNumber(w)}
+              </span>
             </div>
           </div>
         </div>
@@ -176,11 +249,26 @@ const Bonuses = () => {
           <div className="bonuses-table bonuses-table-1">
             <div className="table-col" style={{width: '66%'}}>
               <span className="table-col-title">Кол-во подключенных абонентов</span>
-              <span className="table-col-value">80</span>
+              <span className="table-col-value">
+                {data.connectedAbonents980 + data.connectedAbonents1200}
+              </span>
             </div>
             <div className="table-col">
-              <span className="table-col-title table-col-value-yellow">Премия</span>
-              <span className="table-col-value">80 000</span>
+              <span className="table-col-title table-col-value-yellow">
+                Премия
+                <input
+                  className="editable-input" type="text"
+                  name="bonusPerConnectedAbonent"
+                  value={data.bonusPerConnectedAbonent}
+                  onChange={changeHandler}/>
+                <span style={{textDecoration: 'underline'}}>c</span>
+              </span>
+              <span className="table-col-value">
+                {formatNumber(
+                  (data.connectedAbonents980 + data.connectedAbonents1200)
+                  * data.bonusPerConnectedAbonent
+                )}
+              </span>
             </div>
           </div>
         </div>
@@ -189,11 +277,21 @@ const Bonuses = () => {
           <div className="bonuses-table bonuses-table-1">
             <div className="table-col" style={{width: '66%'}}>
               <span className="table-col-title">4. Премия за Активных абонентов</span>
-              <span className="table-col-value">3000</span>
+              <span className="table-col-value">{data.actives}</span>
             </div>
             <div className="table-col">
-              <span className="table-col-title table-col-value-yellow">Премия</span>
-              <span className="table-col-value">75 000</span>
+              <span className="table-col-title table-col-value-yellow">
+                Премия
+                <input
+                  className="editable-input" type="text"
+                  name="bonusPerActiveAbonent2"
+                  value={data.bonusPerActiveAbonent2}
+                  onChange={changeHandler}/>
+                <span style={{textDecoration: 'underline'}}>c</span>
+              </span>
+              <span className="table-col-value">
+                {formatNumber(data.actives * data.bonusPerActiveAbonent2)}
+              </span>
             </div>
           </div>
         </div>
@@ -202,7 +300,9 @@ const Bonuses = () => {
             <h2 className="table-col-value-yellow" style={{padding: '11px 50px'}}>
               ИТОГО ПРЕМИЯ за текущий месяц на бригаду
             </h2>
-            <h1 style={{color: '#29384A', padding: '9px'}}>206 000 сом</h1>
+            <h1 style={{color: '#29384A', padding: '9px'}}>
+              {formatNumber(bonusForCurrentMonth)} сом
+            </h1>
           </div>
           <div
             className="border-yellow"
@@ -220,13 +320,23 @@ const Bonuses = () => {
               <span style={{color: '#29384A', fontSize: '18px', fontWeight: '600'}}>
                 Для этого надо вернуть всего абонентов из неактивки:
               </span>
-              <h2 style={{marginTop: 'auto', color: '#D1585B'}}>209</h2>
+              <h2 style={{marginTop: 'auto', color: '#D1585B'}}>
+                {Math.ceil((data.actives + data.nonActives) * 0.93 - data.actives)}
+              </h2>
             </div>
             <div className="border-red">
               <h2 className="table-col-value-red" style={{padding: '11px 20px', fontSize: '18px'}}>
                 Вы можете ЗАРАБОТАТЬ ДОПОЛНИТЕЛЬНО если неактивку сократить до 7%
+                <input
+                  className="editable-input" type="text"
+                  name="bonusPerReturnedAbonent2"
+                  value={data.bonusPerReturnedAbonent2}
+                  onChange={changeHandler}/>
+                <span style={{textDecoration: 'underline'}}>c</span>
               </h2>
-              <h1 style={{color: '#29384A', padding: '9px'}}>+ 20 738 сом</h1>
+              <h1 style={{color: '#29384A', padding: '9px'}}>
+                {abonentAmountToReturn > 0 ? `+ ${formatNumber(abonentAmountToReturn)}` : 0} сом
+              </h1>
             </div>
           </div>
         </div>
